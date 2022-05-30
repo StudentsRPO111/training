422250dd988b676fbf16b30de8923e782ff25f27
Alex <gamesevolutionteam@gmail.com>
2022-05-30 20:50:56 +0300

    Garbage clear

--- main.cpp
+++ main.cpp
@@ -1,29 +1,9 @@
 #include <iostream>
 
-
-
 using namespace std;
-int func1(int a){
-    cout << a;
-	return a;
-}
+
 int main() {
 	setlocale(LC_ALL, "rus");
-	int x = 5;
-	cout << "���������: " << x * x << endl;
-	cout << "New ";
-	std::cout << "���� �� ��� �������, �� ������ � ���� ��������, ������ �������-��������" << "kto prochitaet, tot sdoxnet";
-	cout << "New !!!";
-	cout << " *^.^* ";
-	std::cout << "���� �� ��� �������, �� ������ � ���� ��������, ������ �������-��������" << "kto prochitaet, tot sdoxnet";
-	std::cout << "���� �� ��� �������, �� ������ � ���� ��������, ������ �������-��������";
-    int a;
-    cin >> a;
-    cout << a;
-    for(int i = 0; i < a;i++){
-        cout << i;
-    }
-    func1(a);
-	cout << "Hello";
+    
     return 0;
 }
