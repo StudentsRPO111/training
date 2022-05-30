71397e7a11b9ad0ed7658811d028b399d21bc22c
Denis <destinysmile@yandex.ru>
2022-05-30 21:47:53 +0300

    ShowArray

--- main.cpp
+++ main.cpp
@@ -7,6 +7,12 @@ void Square(int a, int b) {
 	cout << square;
 }
 
+void ShowArray(int arr[], int size){
+    for(int i = 0; i < size; i++){
+        cout << arr[i] << "  ";
+    }
+}
+
 int main() {
 	setlocale(LC_ALL, "rus");
 	return 0;
