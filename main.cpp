f991fa52340d5aab86a9bdb10e2d2c0511c70b68
Max <fedotov.m.v@hotmail.com>
2022-05-30 21:34:49 +0300

    create array

--- main.cpp
+++ main.cpp
@@ -2,8 +2,19 @@
 
 using namespace std;
 
+int Create_Array(const int SIZE)
+{
+	int arr[SIZE];
+	for (int i = 0; i < SIZE; i++)
+	{
+		arr[i] = rand() % 11;
+		return arr[i];
+	}
+}
+
 int main() {
 	setlocale(LC_ALL, "rus");
-    
+	const int SIZE = 10;
+	Create_Array(SIZE);
     return 0;
 }
