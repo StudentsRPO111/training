d3d4e6ce9977464f938ce63380432ebca918e90e
Denis <destinysmile@yandex.ru>
2022-05-30 21:41:58 +0300

    ShowArray

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+void ShowArray(int arr[], int size){
+    for(int i = 0; i < size; i++){
+        cout << arr[i] << "  ";
+    }
+}
+
 int main() {
 	setlocale(LC_ALL, "rus");
     
