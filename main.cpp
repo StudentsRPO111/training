a73fd23bbf436551069efa37ded1e44713bdf354
Kazah <khisamutdinovdr@gmail.com>
2022-05-30 20:14:44 +0300

    Soobsheniye

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main() {
-	sertlocale(0, "rus");
-	std::cout << "Если ты это читаешь, то значит я стал сеньором, первым казахом-сеньором";
+	setlocale(0, "rus");
+	std::cout << "Если ты это читаешь, то значит я стал сеньором, первым казахом-сеньором" << "kto prochitaet, tot sdoxnet";
     int a;
     cin >> a;
     cout << a;
