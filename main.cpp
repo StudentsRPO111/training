420753fe59288532da0a86744677de5f8403fa5a
Jem1n1 <78845106+Jem1n1@users.noreply.github.com>
2022-05-30 21:41:59 +0300

    create array (#10)
    * create array
    
    * create array

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
+int* create_array(int SIZE)
+{
+	int *arr = new int[SIZE];
+	for (int i = 0; i < SIZE; i++)
+	{
+		arr[i] = 0;
+	}
+	return arr;
+  
 void Square(int a, int b) {
 	int square = pow(a, b);
 	cout << square;
@@ -9,5 +19,7 @@ void Square(int a, int b) {
 
 int main() {
 	setlocale(LC_ALL, "rus");
+  const int SIZE = 10;
+	create_array(SIZE);
 	return 0;
 }
