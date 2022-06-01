1822e67831556ffd80c05dbd9058815a74ee51f5
Asia <assa.junus@gmail.com>
2022-06-01 21:50:14 +0300

    full game

--- main.cpp
+++ main.cpp
@@ -3,40 +3,18 @@
 
 using namespace std;
 
-int* create_array(int SIZE)
-{
-	int *arr = new int[SIZE];
-	for (int i = 0; i < SIZE; i++)
-	{
-		arr[i] = 0;
+int main(){
+	char field[3][3];
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+		field[i][j] = '0';
+		cout << field[i][j];
+		}
+		cout << endl;
 	}
-	return arr;
-}
 
-void Square(int a, int b) {
-	int square = pow(a, b);
-	cout << square;
-}
 
-void increment(int *a)
-{
-	(*a)++;
-}
 
-void ShowArray(int arr[], int size){
-    for(int i = 0; i < size; i++){
-        cout << arr[i] << "  ";
-    }
-}
-
-void square_array_elements(int* arr[], int size){
-    for(int i = 0; i < size; i++){
-        *arr[i] = *arr[i] * *arr[i];
-    }
-}
-
-int main() {
-	setlocale(LC_ALL, "rus");
 
 	return 0;
 }
