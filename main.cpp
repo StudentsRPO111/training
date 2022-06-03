aa17855c0e40e766238188efab4a2218b5485b3a
Sofi <safiulina.sofia7@gmail.com>
2022-06-03 21:41:26 +0300

    t

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
-#define EMPTY 0
-#define CROSS 1
-#define ZERO 2
-
 using namespace std;
-char win = '-';  //Символ победы
-char user1[80]; char user2[80]; //Переменнные для имен игроков
-char  cells[9] = { '-','-','-','-','-','-', '-','-','-' }; //Здесь будут храниться данные о ячейках
 
-//Выводим поле функцией 
-void pole() {
-	system("cls"); //Очищаем экран, что б было все по красоте
+char player1[80]; char player2[80]; //Переменнные для имен игроков
+char cells[9] = { '-','-','-','-','-','-', '-','-','-' };
+char win = '-';
+void vivod_pole() {
+	system("cls"); //Очищаем экран
 
 	cout << " Числа клеток \n";
 	cout << "-" << 1 << "-" << "|" << "-" << 2 << "-" << "|" << "-" << 3 << "-" << "\n";
@@ -24,52 +19,56 @@ void pole() {
 	cout << "-" << cells[6] << "-" << "|" << "-" << cells[7] << "-" << "|" << "-" << cells[8] << "-" << "\n";
 }
 
-void turn(int number) { //Эта функция ничего не возвращает. У нее есть аргумент - номер игрока, который будет делать ход
-	if (number == 1)
-		cout << user1;
-	else cout << user2;
-	int cell;
-	cout << "Ваш ход! Введите номер ячейки \n";
+void make_move(int numplayer) {
+	if (numplayer == 1) cout << player1;
+	else cout << player2;
+	int cell; //Клетка(ячейка)
+	cout << "Введите номер ячейки \n";
+	cin >> cell;
+	while (cell < 9 || cell < 1 || cells[cell - 1] == 'O' || cells[cell - 1] == 'X')
+		cout << "Ошибка \n";
+	cin >> cell;
 
-
-
-	while (cell > 9 || cell < 1 || cells[cell - 1] == '0' || cells[cell - 1] == 'X') {
-		cout << "Введите номер клетки: \n";
-		cin >> cell;
-		cout << "\n";
-	}
-	//Первый игрок играет крестиками, а второй ноликами 
-	if (number == 1) cells[cell - 1] = 'X';
+	if (numplayer == 1) cells[cell - 1] = 'X';
 	else cells[cell - 1] = 'O';
 }
 
-char check()
-{
-	for (int i = 0; i < 3; i++) //Для победы нужны три клеточки
-		if (cells[i * 3] == cells[i * 3 + 1] && cells[i * 3 + 1] == cells[i * 3 + 2])  //Проверка по горизонтали. Совпадают ли 
+char check() {
+	for (int i = 0; i < 3; i++)
+		if (cells[i * 3] == cells[i * 3 + 1] && cells[i * 3 + 1] == cells[i * 3 + 2])
 			return cells[i];
-		else if (cells[i] == cells[i + 3] && cells[i + 3] == cells[i + 6]) //Проверка по вертикали
+		else if (cells[i] == cells[i + 3] && cells[i + 3] == cells[i + 6])
 			return cells[i];
-		else if ((cells[4] && cells[4] == cells[6]) || (cells[4] && cells[4] == cells[8]))//Проверка по диагонали  
+		else if ((cells[2] == cells[4] && cells[4] == cells[6]) || (cells[0] == cells[4] && cells[4] == cells[8]))
 			return cells[i];
-	return '-'; //Иначе - ничья
-}
+	return '-';
 
-void result() {
+}
+void res() {
 	if (win == 'X')
-		cout << user1 << "Игрок 1 - Мои поздравления! Вы победили! \n ";
+		cout << player1 << "Победил игрок 1 \n";
 	else if (win == 'O')
-		cout << "Игрок 2 - Мои поздравления! Вы победили!\n";
+		cout << player2 << "Победил игрок 2 \n";
 }
 
 
-int main() {
+int main()
+{
 	setlocale(LC_ALL, "Rus");
-	cout << "Добро пожаловать в игру ! \n";
-
-	pole();
-	for (int turn = 1; turn <= 9; turn++) {
-		if (turn % 2)
-	}
+	cout << "Добро пожаловать! \n";
 }
+
+
+void vivod_pole() {
+	if (move >= 5)
+	{
+		win = check();
+		if (win != '-')
+			break;
+	}
+
+}
+
+
+
 
