89531078c17ec5ab82ebf908dd8cf172e8b572ea
Denis2206 <106402797+Denis2206@users.noreply.github.com>
2022-06-08 15:47:31 +0300

    little_changes

---
+++ Hodit_chelovechek_(hw).cpp
@@ -0,0 +1,216 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <ctime>
+#define SIZE 26
+#define UP 72
+#define DOWN 80
+#define LEFT 75
+#define RIGHT 77
+#define ESC 27
+#define SPACE 32
+using namespace std;
+
+struct Entity {
+	int x;
+	int y;
+};
+
+void newpole(char **arr) {
+	const int line_size = SIZE + 2;
+	char *field_view = new char[SIZE*line_size + 1]{ 0 };
+	for (int i = 0; i < SIZE; i++) {
+		field_view[i*line_size] = '\t';
+		for (int j = 0; j < SIZE; j++) {
+			/*cout << arr[i][j] << " ";*/
+			field_view[i*line_size + j + 1] = arr[i][j];
+		}
+		field_view[i*line_size + SIZE + 1] = '\n';
+	}
+	system("cls");
+	cout << field_view;
+}
+
+Entity movement(char **arr, Entity player) {
+	/*char xy;
+	xy = _getch();*/
+	if (!_kbhit())
+		return player;
+	arr[player.x][player.y] = ' ';
+	switch (_getch()) //перед этим было switch(x)
+	{
+		case UP:
+			if (arr[player.x ? player.x - 1 : SIZE - 1][player.y] == '?') break;
+			player.x = player.x ? player.x - 1 : SIZE - 1;
+			break;
+		case DOWN:
+			if (arr[(player.x + 1) % 20][player.y] == '?') break;
+			player.x = (player.x + 1) % 20;
+			break;
+		case LEFT:
+			if (arr[player.x][player.y ? player.y - 1 : SIZE - 1] == '?') break;
+			player.y = player.y ? player.y - 1 : SIZE - 1;
+			break;
+		case RIGHT:
+			if (arr[player.x][(player.y + 1) % 20] == '?') break;
+			player.y = (player.y + 1) % 20;
+			break;
+		case ESC:
+			exit(0);
+			break;
+	}
+	arr[player.x][player.y] = 'X';
+	return player;
+}
+
+Entity enemy_move(char **arr, Entity enemy) {
+	int x_search{ 5 }, y_search{ 5 }, go = 0;
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			if (arr[i][j] == 'X') {
+				x_search = i;
+				y_search = j;
+				go++;
+				break;
+			}
+		}
+		if (go == 1) break;
+	}
+	if (arr[(enemy.x + 1) % 20][enemy.y] == '?' || arr[enemy.x ? enemy.x - 1 : SIZE - 1][enemy.y] == '?') {
+		arr[enemy.x][enemy.y] = ' ';
+		enemy.y = enemy.y ? enemy.y + 1 : enemy.y - 1;
+		arr[enemy.x][enemy.y] = '#';
+	}
+	else if (arr[enemy.x][(enemy.y + 1) % 20] == '?' || arr[enemy.x][enemy.y ? enemy.y - 1 : SIZE - 1] == '?') {
+		arr[enemy.x][enemy.y] = ' ';
+		enemy.x = enemy.x ? enemy.x + 1 : enemy.x - 1;
+		arr[enemy.x][enemy.y] = '#';
+	}
+	else {
+		arr[enemy.x][enemy.y] = ' ';
+		if (enemy.x == x_search) {
+			enemy.y = enemy.y < y_search ? enemy.y + 1 : enemy.y - 1;
+		}
+		else enemy.x = enemy.x < x_search ? enemy.x + 1 : enemy.x - 1;
+		arr[enemy.x][enemy.y] = '#';
+	}
+	return enemy;
+}
+
+bool check_kill(Entity player, Entity enemy) {
+	if (player.x == enemy.x && player.y == enemy.y) {
+		cout << "\n\tТебя поймали, ты проиграл!\n";
+		return true;
+	}
+	return false;
+}
+
+BOOL ShowConsoleCursor(BOOL bShow) {
+	CONSOLE_CURSOR_INFO cci;
+	HANDLE hStdOut;
+	hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (hStdOut == INVALID_HANDLE_VALUE)
+		return FALSE;
+	if (!GetConsoleCursorInfo(hStdOut, &cci))
+		return FALSE;
+	cci.bVisible = bShow;
+	if (!SetConsoleCursorInfo(hStdOut, &cci))
+		return FALSE;
+	return TRUE;
+}
+
+
+Entity construct_map(char** arr, Entity cursor) {
+	if (arr[cursor.x][cursor.y] != '?') arr[cursor.x][cursor.y] = ' ';
+	switch (_getch()) {
+		case UP:
+			cursor.x = cursor.x ? cursor.x - 1 : SIZE - 1;
+			if (arr[cursor.x][cursor.y] != '?') arr[cursor.x][cursor.y] = '<';
+			break;
+		case DOWN:
+			cursor.x = (cursor.x + 1) % 20;
+			if (arr[cursor.x][cursor.y] != '?') arr[cursor.x][cursor.y] = '<';
+			break;
+		case LEFT:
+			cursor.y = cursor.y ? cursor.y - 1 : SIZE - 1;
+			if (arr[cursor.x][cursor.y] != '?') arr[cursor.x][cursor.y] = '<';
+			break;
+		case RIGHT:
+			cursor.y = (cursor.y + 1) % 20;
+			if (arr[cursor.x][cursor.y] != '?') arr[cursor.x][cursor.y] = '<';
+			break;
+		case ESC:
+			cursor.x = -10;
+			break;
+		case SPACE:
+			if (arr[cursor.x][cursor.y] != '?')
+				arr[cursor.x][cursor.y] = '?';
+			else
+				arr[cursor.x][cursor.y] = '<';
+			return cursor;
+	}
+	return cursor;
+}
+
+int main() {
+	HANDLE out_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD crd = { 60, 30 };
+	SMALL_RECT src = { 0, 0, crd.X - 1, crd.Y - 1 };
+	SetConsoleWindowInfo(out_handle, true, &src);
+	SetConsoleScreenBufferSize(out_handle, crd);
+
+	setlocale(LC_ALL, "RUS");
+	unsigned int delay_time;
+	char a;
+	char yes[5];
+	int delay_step = 0;
+	Entity player = { 15, 15 };
+	Entity enemy1 = { 0,0 };
+	Entity enemy2 = { 25,25 };
+	Entity cursor = { 15,15 };
+	char **field = new char*[SIZE];
+	for (int i = 0; i < SIZE; i++) {
+		field[i] = new char[SIZE];
+		for (int j = 0; j < SIZE; j++)
+			field[i][j] = ' ';
+	}
+	cout << "\n\tХотите нарисовать карту? (yes/no)\n";
+	cin >> yes;
+	_strlwr(yes);
+	ShowConsoleCursor(FALSE);
+	if ((_strnicmp((yes), "yes", 3) == 0)) {
+		while (true) {
+
+			cursor = construct_map(field, cursor);
+			newpole(field);
+			if (cursor.x == -10) break;
+		}
+	}
+	else ((_strnicmp((yes), "no", 2) == 0));
+
+	field[player.x][player.y] = 'X';
+	field[enemy1.x][enemy1.y] = '#';
+	field[enemy2.x][enemy2.y] = '%';
+	newpole(field);
+
+	while (!check_kill(player, enemy1) && !check_kill(player, enemy2)) {
+		player = movement(field, player);
+		delay_time = clock();
+		if (delay_step == 5) {
+			if (delay_time % 600 >= 0 && delay_time % 600 <= 100) {
+				enemy1 = enemy_move(field, enemy1);
+				enemy2 = enemy_move(field, enemy2);
+			}
+			delay_step = 0;
+		}
+		delay_step++;
+		//Sleep(200);
+		newpole(field);
+	}
+	ShowConsoleCursor(TRUE);
+
+	_getch();
+	return 0;
+}
