45a315d23e1708cf58ce7d2650edfd0c4078a26f
Jem1n1 <78845106+Jem1n1@users.noreply.github.com>
2022-07-01 21:10:34 +0300

    Add files via upload

---
+++ ООП ВВОД.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Point {
+public:
+	//сеттеры
+	/*void setX(int value) {
+		x = value;
+	}
+	void setY(int value) {
+		y = value;
+	}
+	void setZ(int value) {
+		z = value;
+	}*/
+	//геттеры
+	/*int getX() {
+		return x;
+	}
+	int getY() {
+		return y;
+	}
+	int getZ() {
+		return z;
+	}*/
+	//конструктор
+	Point(int x, int y, int z) : x{ x }, y{ y }, z{ z } {}
+	//метод
+	void outputPoint() {
+		cout << "Точка Х: " << x << endl;
+		cout << "Точка Y: " << y << endl;
+		cout << "Точка Z: " << z << endl;
+	}
+
+private:
+	int x = 0;
+	int y = 0;
+	int z = 0;
+};
+
+int main()
+{ 
+	setlocale(LC_ALL, "rus");
+	int x, y, z;
+	cin >> x;
+	cin >> y;
+	cin >> z;
+	//объект класса
+	Point p(x, y, z);
+	/*p.setX(x);
+	p.setY(y);
+	p.setZ(z);
+	cout << p.getX() << " " << p.getY() << " " << p.getZ();*/
+	p.outputPoint();
+}
