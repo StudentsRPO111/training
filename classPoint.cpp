bcfe5afb4fa4354d7984035afd6557e180f731f0
Jem1n1 <78845106+Jem1n1@users.noreply.github.com>
2022-07-04 21:11:01 +0300

    Add files via upload

---
+++ classPoint.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Tochka
+{
+public:
+	int x;
+	int y;
+	Tochka(int x, int y) : Tochka() {
+
+	}
+	~Tochka() {
+		objects_count--;
+	}
+	static int const &ObjectsCount() {
+		return objects_count;
+	};
+private:
+	static int objects_count;
+	Tochka() {
+		objects_count++;
+	}
+
+};
+
+int Tochka::objects_count = 0;
+
+int main()
+{
+	Tochka *p = new Tochka(5,6);
+	cout << p->ObjectsCount();
+	delete p;
+	cout << endl << p->ObjectsCount();
+}
